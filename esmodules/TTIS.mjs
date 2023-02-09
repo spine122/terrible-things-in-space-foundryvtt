3b27b02eacb72a6f4e002618d38cdf36de00d98a
spine <wesley_jensen122@yahoo.com>
2023-02-09 00:36:39 -0800

    first version, nothing to see here yet.

---
+++ esmodules/TTIS.mjs
@@ -0,0 +1,11 @@
+// Import Document Classes
+import { TerriblePC } from "./documents/PC.mjs";
+import { TerribleGear } from "./documents/gear.mjs";
+
+//Import sheet classes
+import { TerribleGearSheet } from "./sheets/gear-sheet.mjs"
+import { TerriblePCSheet } from "./sheets/PC-sheet.mjs"
+
+//Import utility classes and constants
+import { preloadHandlebarsTemplates } from "./helpers/templates.mjs"
+import { TTIS } from "./helpers/config.mjs"
